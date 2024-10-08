c2c3ccc85f218929f71032512ef49ce4669311d2
Zer <livefatezz@gmail.com>
2024-10-08 21:21:01 +0800

    feat: implement web3auth with solana

--- frontend/src/types/user.d.ts
+++ frontend/src/types/user.d.ts
@@ -1,12 +1,13 @@
 import { AuthUserInfo } from "@web3auth/auth-adapter";
 import { PublicClient, WalletClient } from "viem";
 import { BOKWGeoABI } from "@/abis/BOKWGeoABI";
+import { Connection } from "@solana/web3.js";
 
 export type Web3AuthContextType = {
   isLoading: boolean;
   user: Partial<AuthUserInfo & { address: `0x${string}` }> | undefined;
-  viemPublicClient?: PublicClient;
-  viemWalletClient?: WalletClient;
+  solanaConnection?: Connection;
+  solanaWallet?: SolanaWallet;
   login: () => Promise<void>;
   logout: () => Promise<void>;
   authenticateUser: () => Promise<UserAuthInfo | undefined>; // get token ID
